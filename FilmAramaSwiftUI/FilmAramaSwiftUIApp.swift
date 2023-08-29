bb55eb381abcdc4e7a21f127f499fbc230d3a72b
exiw <51192517+exiw@users.noreply.github.com>
2023-08-29 22:06:13 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by Emre Konukpay on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
